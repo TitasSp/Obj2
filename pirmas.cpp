5d35f89ec9456bd9f8f09b3bd55f190867bbdfd5
TitasSp <titas.spegis@mif.stud.vu.lt>
2025-02-12 21:51:49 +0200

    added set precision
    fixed issue with pazSk not updating correctly and added set precision to output

--- pirmas.cpp
+++ pirmas.cpp
@@ -10,6 +10,8 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using std::fixed;
+using std::setprecision;
 
 struct Studentas {
     string vardas;
@@ -17,6 +19,7 @@ struct Studentas {
     vector<int> pazymiai;
     int egzaminas;
     float galutinis;
+    int pazSk; // pazymiu skaicius
 };
 
 float Vidurkis(vector<int> pazymiai, int pazSk);
@@ -24,7 +27,7 @@ float Vidurkis(vector<int> pazymiai, int pazSk);
 
 int main() {
     int n; // studentu skaicius
-    int pazSk = 0; // pazymiu skaicius
+    
     cout << "Iveskite studentu skaiciu: ";
     cin >> n;
     vector<Studentas> studentas(n);
@@ -35,20 +38,21 @@ int main() {
         cin >> studentas[i].vardas;
         cout << "Iveskite studento pavarde: ";
         cin >> studentas[i].pavarde;
-        cout << "Iveskite pazymiu skaiciu: " << endl;
-        cin >> pazSk;
-        studentas[i].pazymiai.resize(pazSk); // pazymiu vektoriui priskiriam dydi
+        cout << "Iveskite pazymiu skaiciu: ";
+        cin >> studentas[i].pazSk;
+        studentas[i].pazymiai.resize(studentas[i].pazSk); // pazymiu vektoriui priskiriam dydi
         cout << "Iveskite studento pazymius: ";
-        for (int j = 0; j < pazSk; j++) {
+        for (int j = 0; j < studentas[i].pazSk; j++) {
             cin >> studentas[i].pazymiai[j];
         }
         cout << "Iveskite studento egzamino pazymi: ";
         cin >> studentas[i].egzaminas;
+        
     }
 
     for (int i = 0; i < n; i++) {
-        studentas[i].galutinis = 0.4 * Vidurkis(studentas[i].pazymiai, pazSk) + 0.6 * studentas[i].egzaminas;
-        cout << studentas[i].vardas << " " << studentas[i].pavarde << " " << studentas[i].galutinis << endl;
+        studentas[i].galutinis = 0.4 * Vidurkis(studentas[i].pazymiai, studentas[i].pazSk) + 0.6 * studentas[i].egzaminas;
+        cout << fixed << setprecision(2) << studentas[i].vardas << " " << studentas[i].pavarde << " " << studentas[i].galutinis << endl;
     }
 
     return 0;
