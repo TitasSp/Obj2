f1bc5c3f321d5784b3ac72a5433007ceed604f32
TitasSp <titas.spegis@mif.stud.vu.lt>
2025-02-12 21:31:41 +0200

    user input added
    User can input: amount of students, names, surnames, amount of grades, grades.

--- pirmas.cpp
+++ pirmas.cpp
@@ -12,23 +12,52 @@ using std::string;
 using std::vector;
 
 struct Studentas {
-    string vardas = "titas";
-    string pavarde = "spegis";
-    vector<int> pazymiai = {10, 9, 8, 7, 6};
-    int egzaminas = 8;
+    string vardas;
+    string pavarde;
+    vector<int> pazymiai;
+    int egzaminas;
     float galutinis;
 };
 
-int main() {
-    Studentas studentas;
+float Vidurkis(vector<int> pazymiai, int pazSk);
 
-    float suma = 0;
-    for (int i = 0; i < 5; i++) {
-        suma += studentas.pazymiai[i];
+
+int main() {
+    int n; // studentu skaicius
+    int pazSk = 0; // pazymiu skaicius
+    cout << "Iveskite studentu skaiciu: ";
+    cin >> n;
+    vector<Studentas> studentas(n);
+
+    //loopina per visus studentus ir iveda ju duomenis
+    for (int i = 0; i < n; i++) {
+        cout << "Iveskite studento varda: ";
+        cin >> studentas[i].vardas;
+        cout << "Iveskite studento pavarde: ";
+        cin >> studentas[i].pavarde;
+        cout << "Iveskite pazymiu skaiciu: " << endl;
+        cin >> pazSk;
+        studentas[i].pazymiai.resize(pazSk); // pazymiu vektoriui priskiriam dydi
+        cout << "Iveskite studento pazymius: ";
+        for (int j = 0; j < pazSk; j++) {
+            cin >> studentas[i].pazymiai[j];
+        }
+        cout << "Iveskite studento egzamino pazymi: ";
+        cin >> studentas[i].egzaminas;
     }
-    float vidurkis = suma / 5;
-    studentas.galutinis = 0.4 * vidurkis + 0.6 * studentas.egzaminas;
-    cout << studentas.vardas << " " << studentas.pavarde << " " << studentas.galutinis << endl;
+
+    for (int i = 0; i < n; i++) {
+        studentas[i].galutinis = 0.4 * Vidurkis(studentas[i].pazymiai, pazSk) + 0.6 * studentas[i].egzaminas;
+        cout << studentas[i].vardas << " " << studentas[i].pavarde << " " << studentas[i].galutinis << endl;
+    }
 
     return 0;
+}
+
+float Vidurkis(vector<int> pazymiai, int pazSk) {
+    float suma = 0;
+    for (int i = 0; i < pazSk; i++) {
+        suma += pazymiai[i];
+    }
+    return suma / pazSk;
 }
