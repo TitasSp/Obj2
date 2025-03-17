5079e9d0353e70fe8271457e7c78011920b5fa00
TitasSp <titas.spegis@mif.stud.vu.lt>
2025-03-17 12:47:30 +0200

    template added
    changed from vector to general template for more flexibility

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include "funkcijos.h"
 
 int main() {
-
+    Test1();
+    for(int i = 0; i < 5; i++) {
+        cout << "Testas " << i + 1 << endl;
+        Test2();
+    }
     try {
         auto start = std::chrono::high_resolution_clock::now(); // Start timer
         std::chrono::duration<double> inputWaitDuration(0); // input laukimo laikas
